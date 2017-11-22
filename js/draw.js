4de19dbb34d30c6c59d23bfc1fd25ca7e7abb84d
alexandre <kalambik@gmail.com>
2017-11-22 16:40:18 +0100

    draw function + canvas

--- js/draw.js
+++ js/draw.js
@@ -2,11 +2,23 @@
 
 $(window).ready(function() {
 
-    //Canvas
-    context = document.getElementById('canvas').getContext("2d");
 
-    //Mouse down event
-    $('#canvas').mousedown(function(e){
+    //Canvas
+    var canvas = document.getElementById('canvas'),
+    context = canvas.getContext("2d");
+
+    window.addEventListener('resize', resizeCanvas, false);
+
+    function resizeCanvas() {
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+      draw(); 
+    }
+    resizeCanvas();
+
+    function draw() {
+      //Mouse down event
+      $('#canvas').mousedown(function(e){
         let mouseX = e.pageX - this.offsetLeft;
         let mouseY = e.pageY - this.offsetTop;
 
@@ -14,52 +26,61 @@ $(window).ready(function() {
 
         addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
         redraw();
-    });
+      });
 
-    //Mouse move event
-    $('#canvas').mousemove(function(e){
-      if(paint){
-        addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
-        redraw();
-    }
-});
+      //Mouse move event
+      $('#canvas').mousemove(function(e){
+        if(paint){
+          addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
+          redraw();
+        }
+      });
 
-    //Mouse leave event
-    $('#canvas').mouseup(function(e){
-      paint = false;
-  });
-    //Save click position 
-    let clickX = new Array();
-    let clickY = new Array();
-    let clickDrag = new Array();
-    let paint;
-    
-    function addClick(x, y, dragging){
-      clickX.push(x);
-      clickY.push(y);
-      clickDrag.push(dragging);
-  }
-  function redraw(){
-    // Clears the canvas
-    context.clearRect(0, 0, context.canvas.width, context.canvas.height); 
+      //Mouse leave event
+      $('#canvas').mouseup(function(e){
+        paint = false;
+      });
+      //Save click position 
+      let clickX = new Array();
+      let clickY = new Array();
+      let clickDrag = new Array();
+      let paint;
 
-    context.strokeStyle = "#df4b26";
-    context.lineJoin = "round";
-    context.lineWidth = 20;
+      function addClick(x, y, dragging){
+        clickX.push(x);
+        clickY.push(y);
+        clickDrag.push(dragging);
+      }
+      function redraw(){
+        // Clears the canvas x, y, width, height
+        context.clearRect(0, 0, context.canvas.width, context.canvas.height); 
 
-    for(var i=0; i < clickX.length; i++) {        
-        context.beginPath();
-        if(clickDrag[i] && i){
-          context.moveTo(clickX[i-1], clickY[i-1]);
-      }else{
-         context.moveTo(clickX[i]-1, clickY[i]);
-     }
-     context.lineTo(clickX[i], clickY[i]);
-     context.closePath();
-     context.stroke();
- }
-}
+        context.strokeStyle = "#df4b26";
+        context.lineJoin = "round";
+        context.lineWidth = 10;
 
-});
+        for(var i=0; i < clickX.length; i++) {        
+          context.beginPath();
+          if(clickDrag[i] && i){
+            context.moveTo(clickX[i-1], clickY[i-1]);
+          }else{
+           context.moveTo(clickX[i]-1, clickY[i]);
+         }
+         context.lineTo(clickX[i], clickY[i]);
+         context.closePath();
+         context.stroke();
+       }
+
+        }//end redraw
+
+ // $("#gomme").click(function() {
+ //        canvas.drawColor(0, Mode.CLEAR);
+ //    });
+
+ 
+};//end function draw()
+
+});//end win ready
+
 
 
