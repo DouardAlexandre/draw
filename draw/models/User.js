647ce2d51c93b6dc721d71c7e1b78e8e0f599b78
Unknown <kalambik@gmail.com>
2017-12-07 12:48:32 +0100

    auth folder

---
+++ draw/models/User.js
@@ -0,0 +1,14 @@
+//Mongoose.js User Model
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var passportLocalMongoose = require('passport-local-mongoose');
+
+var UserSchema = new Schema({
+    username: String,
+    password: String,
+    name: String
+});
+
+UserSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('User', UserSchema);
